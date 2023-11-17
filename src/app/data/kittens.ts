3e66c45eb40eb17e67797b4506ccecd49908ff39
khownz <koen895@hotmail.com>
2023-11-17 22:36:17 +0100

    added Odette & placeholders

--- src/app/data/kittens.ts
+++ src/app/data/kittens.ts
@@ -13,4 +13,44 @@ export const KITTENS: Cat[] = [
       '🎉 Spelen en ravotten doet ze graag en ze heeft duidelijk meer energie dan ik. Dat zal komen doordat ze wat groter en sterker is.',
     imagePathNames: ['assets/kittens/opoes-ophelia-1.png', 'assets/kittens/opoes-ophelia-2.png'],
   },
+  {
+    name: 'Odette',
+    bio: 'TODO',
+    imagePathNames: ['assets/kittens/odette-1.jpg', 'assets/kittens/odette-2.jpg'],
+  },
+  {
+    name: '',
+    bio: 'TODO',
+    imagePathNames: [],
+  },
+  {
+    name: '',
+    bio: 'TODO',
+    imagePathNames: [],
+  },
+  {
+    name: '',
+    bio: 'TODO',
+    imagePathNames: [],
+  },
+  {
+    name: '',
+    bio: 'TODO',
+    imagePathNames: [],
+  },
+  {
+    name: '',
+    bio: 'TODO',
+    imagePathNames: [],
+  },
+  {
+    name: '',
+    bio: 'TODO',
+    imagePathNames: [],
+  },
+  {
+    name: '',
+    bio: 'TODO',
+    imagePathNames: [],
+  },
 ];
