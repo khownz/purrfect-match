ff15072b381dea5229067d6ae56dffe136ad3418
khownz <koen895@hotmail.com>
2023-11-17 22:18:13 +0100

    added first kitten with images

--- src/app/assets/kittens.ts
+++ src/app/assets/kittens.ts
@@ -1,3 +1,9 @@
 import { Cat } from '../types/cat';
 
-export const KITTENS: Cat[] = [];
+export const KITTENS: Cat[] = [
+  {
+    name: 'Opoes & Ophelia',
+    bio: 'TODO',
+    imagePathNames: ['assets/kittens/opoes-ophelia-1.png', 'assets/kittens/opoes-ophelia-2.png'],
+  },
+];
