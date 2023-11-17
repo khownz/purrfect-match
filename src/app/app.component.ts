1e5317d7d638015fb0d9c4583f80f716f186c2a1
khownz <koen895@hotmail.com>
2023-11-17 22:50:06 +0100

    Display first kitten image + remove angular cache

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { KITTENS } from './data/kittens';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'purrfect-match';
+  activeCat = KITTENS[0];
 }
