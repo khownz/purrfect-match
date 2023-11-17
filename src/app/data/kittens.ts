14335706dcb1cc036a56ae9a4a6eb84420f63efe
khownz <koen895@hotmail.com>
2023-11-18 00:29:36 +0100

    Added all pics for kittens & bio for Ricky & Roxy

--- src/app/data/kittens.ts
+++ src/app/data/kittens.ts
@@ -2,6 +2,25 @@ import { Cat } from '../types/cat';
 
 export const KITTENS: Cat[] = [
   {
+    name: 'Ricky & Roxy',
+    bio:
+      '🐾 Ricky en Roxy, een ondeugend broer-zus duo op ontdekkingstocht in de wereld.<br/>' +
+      '🐱 Voor Roxy lijkt dat niet evident met drie pootjes, maar als je haar rond ziet lopen, merk je er niets van.<br/>' +
+      '🤝 Sociale kittens die graag spelen met elkaar en andere katten, altijd van de partij als je op de zetel zit.<br/>' +
+      '🛋️ Geen typische schootkatten, Ricky laat zich regelmatig aaien maar heeft ook zijn rustmomenten nodig.<br/>' +
+      '🐾 Roxy laat liever niet aaien, maar ze laat het toe als ze je eenmaal kent en durft dan al wel eens te spinnen.<br/>' +
+      '🚀 Ricky vliegt meteen rond in je huis, wil je meteen leren kennen. 🕰️ Roxy heeft meer tijd nodig, kijkt in het begin de kat uit de boom (pun unintended), maar het is leuk haar vooruitgang te zien maken.<br/>' +
+      '🚸 Vanwege hun rustmomenten raden we hen niet aan voor gezinnen met jonge kinderen.<br/>' +
+      '🌳 Ze kunnen ook de buitenwereld verkennen als je dat wil, maar voor Roxy is een afgeschermde omgeving het beste.',
+    imagePathNames: [
+      'assets/kittens/ricky-roxy-1.jpeg',
+      'assets/kittens/ricky-roxy-2.jpeg',
+      'assets/kittens/ricky-roxy-3.jpeg',
+      'assets/kittens/ricky-roxy-4.jpeg',
+      'assets/kittens/ricky-roxy-5.jpeg',
+    ],
+  },
+  {
     name: 'Opoes & Ophelia',
     bio:
       '🐾 Ik ben Opoes, een zwart kattinnetje met witte snoet en witte pootjes.<br/>' +
@@ -15,37 +34,47 @@ export const KITTENS: Cat[] = [
   },
   {
     name: 'Odette',
-    bio: 'TODO 2',
+    bio: 'TODO',
     imagePathNames: ['assets/kittens/odette-1.jpg', 'assets/kittens/odette-2.jpg'],
   },
   {
-    name: '',
-    bio: 'TODO 3',
-    imagePathNames: [],
+    name: 'Jools & Joey',
+    bio: 'TODO',
+    imagePathNames: [
+      'assets/kittens/jools-joey-1.png',
+      'assets/kittens/jools-joey-2.png',
+      'assets/kittens/jools-joey-3.png',
+      'assets/kittens/jools-joey-4.png',
+      'assets/kittens/jools-joey-5.png',
+      'assets/kittens/jools-joey-6.png',
+      'assets/kittens/jools-joey-7.png',
+    ],
   },
   {
-    name: '',
-    bio: 'TODO 4',
-    imagePathNames: [],
+    name: 'Dora',
+    bio: 'TODO',
+    imagePathNames: ['assets/kittens/dora-1.jpg', 'assets/kittens/dora-2.jpg', 'assets/kittens/dora-3.jpg'],
   },
   {
-    name: '',
-    bio: 'TODO 5',
-    imagePathNames: [],
+    name: 'Frankie & Fen',
+    bio: 'TODO',
+    imagePathNames: [
+      'assets/kittens/frankie-fen-1.jpg',
+      'assets/kittens/frankie-fen-2.jpeg',
+      'assets/kittens/frankie-fen-3.jpeg',
+      'assets/kittens/frankie-fen-4.jpeg',
+    ],
   },
   {
-    name: '',
-    bio: 'TODO 6',
-    imagePathNames: [],
-  },
-  {
-    name: '',
-    bio: 'TODO 7',
-    imagePathNames: [],
-  },
-  {
-    name: '',
-    bio: 'TODO 8',
-    imagePathNames: [],
+    name: 'Simba & Scooby',
+    bio: 'TODO',
+    imagePathNames: [
+      'assets/kittens/simba-scooby-1.jpg',
+      'assets/kittens/simba-scooby-2.jpg',
+      'assets/kittens/simba-scooby-3.jpg',
+      'assets/kittens/simba-scooby-4.jpg',
+      'assets/kittens/simba-scooby-5.jpg',
+      'assets/kittens/simba-scooby-6.png',
+    ],
   },
 ];
