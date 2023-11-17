23f24f4f220c5acd2aef58f6e52f7c52a5cbae64
khownz <koen895@hotmail.com>
2023-11-17 22:26:28 +0100

    Added first kitten

---
+++ src/app/data/kittens.ts
@@ -0,0 +1,16 @@
+import { Cat } from '../types/cat';
+
+export const KITTENS: Cat[] = [
+  {
+    name: 'Opoes & Ophelia',
+    bio:
+      '🐾 Ik ben Opoes, een zwart kattinnetje met witte snoet en witte pootjes.\n' +
+      '🌟 Ontdekken doe ik met veel plezier, al moet ik toegeven dat dat dutje erna wel nog veel deugd doet.\n' +
+      '😺 Wat wil je, ik word zo vlug moe van al het spelen en ontdekken… dan zoek ik geborgenheid bij broertjes en zusjes.\n' +
+      '🛌 Gezellig hé, ik slaap het liefst omgeven door hun.\n' +
+      '🐱 Mijn zusje Ophelia is ook een zwart-wit kattinnetje. Ze laat me niet vlug uit het lood slaan, nee hoor, ze staat haar mannetje!\n' +
+      '🤫 Ze heeft gezegd dat ik dit eigenlijk niet mag zeggen, maar stiekem knuffelt ze wel heel graag met haar pleegmama (die is hier helemaal zot van!).\n' +
+      '🎉 Spelen en ravotten doet ze graag en ze heeft duidelijk meer energie dan ik. Dat zal komen doordat ze wat groter en sterker is.',
+    imagePathNames: ['assets/kittens/opoes-ophelia-1.png', 'assets/kittens/opoes-ophelia-2.png'],
+  },
+];
--- src/assets/kittens.ts
+++
@@ -1,9 +0,0 @@
-import { Cat } from '../app/types/cat';
-
-export const KITTENS: Cat[] = [
-  {
-    name: 'Opoes & Ophelia',
-    bio: 'TODO',
-    imagePathNames: ['assets/kittens/opoes-ophelia-1.png', 'assets/kittens/opoes-ophelia-2.png'],
-  },
-];
