08b3c9c3bc915929e42f7ed57c1f0a31f331b5ad
wouterroothooft <wouter.roothooft@jidoka.be>
2023-11-18 19:04:45 +0100

    Dark theme added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DrawerComponent } from './components/drawer/drawer.component';
+import { ThemeService } from './core/services/theme/theme.service';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [CommonModule, RouterOutlet, DrawerComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, DrawerComponent],
+  template: ` <router-outlet></router-outlet> `,
 })
 export class AppComponent {
+  constructor(themeService: ThemeService) {
+    themeService.configureActiveTheme();
+  }
 }
